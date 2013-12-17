48af813849c63ee1b1956a44f6ff90bef67877a0
pavelDeminov <sindanar@gmail.com>
2013-12-18 00:24:55 +0600

    Added guy test

---
+++ UnityProject/Assets/Resources/scripts/GUIController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIController : MonoBehaviour {
+	
+	public int healthValue;
+	private GameObject healthObject;
+	private int testHealthChange = 1;
+	private GUITexture healthTexture;
+	// Use this for initialization
+	void Start () {
+		
+		healthObject = GameObject.Find("GUI/g_health");
+		healthTexture = healthObject.GetComponent<GUITexture>();
+		
+		healthValue = 100;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (healthValue>100 || healthValue<0) {
+			testHealthChange = -testHealthChange;	
+			
+		}
+		healthValue+=testHealthChange;
+		Rect  frame = healthTexture.pixelInset;
+		frame.height = healthValue*(Screen.height/100.0f);
+		healthTexture.pixelInset = frame;
+		
+	
+	
+	}
+}
